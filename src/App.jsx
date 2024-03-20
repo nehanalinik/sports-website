94a62f73ea318d84cbf6133368add03697079ef1
neha-kennedy <nehanalinik@gmail.com>
2024-03-20 16:49:39 +0530

    added new background

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ function App() {
   return (
     <section>
       <div className="background"></div>
-      <div className="overflow-auto h-[94vh] bg-white/30 z-10 m-5 rounded-md scroll-smooth">
+      <div className="overflow-auto h-[94vh] bg-white/10 z-10 m-5 rounded-md scroll-smooth">
         <div className="flex flex-col p-4 items-center justify-center">
           <div className="m-4 flex items-center gap-2 w-[100%]">
             <img src={Logo} alt="logo" width={110} />
             <div className=" w-[100%] flex items-center flex-col">
-              <div className="text-md xsm:text-lg sm:text-xl smd:text-2xl md:text-3xl lg:text-4xl text-slate-900">
+              <div className="text-md xsm:text-lg sm:text-xl smd:text-2xl md:text-3xl lg:text-4xl text-slate-200">
                 BPBO Presents
               </div>
-              <div className="text-sm xsm:text-md sm:text-lg smd:text-xl md:text-2xl font-bold text-white/90 tracking-wide">
+              <div className="text-sm xsm:text-md sm:text-lg smd:text-xl md:text-2xl font-bold text-slate-200 tracking-wide">
                 2024-25
               </div>
             </div>
